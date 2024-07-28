6628bdf230456b06a036f24deff171bc69e49974
Arslan Tariq <arslantariq4546@gmail.com>
2024-07-29 00:23:10 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:login_signup_ui_design/screens/SignUpScreen.dart';
+
+void main() => runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignUpScreen(),
+    )
+);
